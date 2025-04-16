f8d810a368e7c0e7186ff9c7f7f7bcd97affdb53
A-J-Ln <alicia.lin@edu.sait.ca>
2025-04-16 15:54:04 -0600

    Updated Index, Script, and CSS
    - added second button
    - added button function
    - changed format
    - added button customization
    - added button animation

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 console.log("Hello World");
 
+// Alert Button
 document.getElementById("alertMe").onclick = function() {
     alert("Akina Nakamori is my favourite \(*' v ' *)");
-  };  
+    }; 
+
+// Hover Button
+document.getElementById("hoverButton").onclick = function() {
+    alert("While not technically CityPop, Masayoshi Takanaka's Rainbow Goblin Album had influence on the genre");
+    };     
+document.getElementById("hoverButton").onmouseover = function() {
+    document.getElementById('hoverButton').style.background = "#235182"; // Change to dark blue on hover
+    };
+document.getElementById("hoverButton").onmouseleave = function() {
+    document.getElementById("hoverButton").style.background = "#3171b5";  // Return to original color when cursor leaves
+    };
